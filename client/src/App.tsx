afd2031c2b4c31a54deb422c6f38bf7813c9a415
Lauraine Baffot <74086642+Lauraine2112@users.noreply.github.com>
2022-03-11 00:48:50 -0700

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import CreateNewBoard from "./CreateNewBoard/CreateNewBoard";
+import Homepage from "./Homepage/Homepage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import logo from "./logo.svg";
+import "./App.css";
+
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001/bulletin-be82d/us-central1/helloWorld"
+    : "https://us-central1-bulletin-be82d.cloudfunctions.net/helloWorld";
+
+function App() {
+  const [serverResponse, setServerResponse] = useState("no server response");
+
+  const handleClick = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setServerResponse(JSON.stringify(data));
+        console.log(JSON.stringify(data));
+      });
+  };
+  const navigation = {
+    brand: { name: 'BULLETIN', to: '/homepage' },
+    links: [
+      { name: 'Home', to: '/homepage' },
+      { name: 'Log In', to: '/login' },
+      { name: 'Sign Up', to: '/signup' }
+    ]
+  };
+
+  const { brand, links } = navigation;
+  return (
+    <div className="App">
+      {/* <Header brand={brand} links={links} /> */}
+      <div>
+        <Homepage />
+        {/* <CreateNewBoard /> */}
+      </div>
+      {/* {<Footer />} */}
+    </div>
+  );
+}
+
+export default App;
