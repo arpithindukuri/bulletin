d2d5eb345a0fe24df61aba1572f32650850703cc
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-03-24 10:55:43 -0600

    No need for the new window the modal appears in the same one

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,8 +9,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import Calendar from "./pages/Calendar";
 import List from "./pages/List";
-import AddEvent from "./pages/AddEvent";
-
 
 const url =
   process.env.NODE_ENV === "development"
@@ -40,7 +38,6 @@ function App() {
      <Route path='/signup' element={<Signup/>}></Route>
      <Route path='/calendar' element={<Calendar/>}></Route>
      <Route path='/list' element={<List/>}></Route>
-     <Route path='/addevent' element={<AddEvent/>}></Route>
    </Routes>
    </BrowserRouter>
    <Footer/>
