cc2836cd3c350a694e26cfd101f3d5ce57b7f3ab
ahmedw14 <ahmedw1114@gmail.com>
2022-03-24 22:18:53 -0600

    Add Notes work but get Notes doesn't

--- client/src/pages/Notes/Notes.tsx
+++ client/src/pages/Notes/Notes.tsx
@@ -5,7 +5,16 @@ import NoteRow from "./NoteRow";
 import "./Notes.css";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../axios";
+import {Snackbar, useForkRef} from "@material-ui/core";
+import { Alert, AlertColor } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
+interface NotesProp {
+  name: string;
+  date: string;
+  tags: string[];
+  content: string;
+}
 const mockNotes = [
   {
     id: 1,
@@ -37,46 +46,126 @@ const mockNotes = [
     content: "note content 1",
   },
 ];
-
+interface State {
+  name: string;
+  date: string;
+  tags: string[];
+  content: string;
+}
+interface CreateNoteErrors {
+  name: string;
+  content: string;
+}
 const Notes: React.FC = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const [popupState, setPopupState] = useState(false);
   const [boardName, setBoardName] = useState("");
+  const [notes, setNotes] = useState<Array<NotesProp>>([]);
+  
+  const [values, setValues] = React.useState<State>({
+    name: "",
+    date: "",
+    tags: [],
+    content: ""
+  });
+  const [errors, setErrors] = useState<CreateNoteErrors>({
+    name: "",
+    content: ""
+  });
+  const [message, setMessage] = useState("");
+  const [messageOpen, setMessageOpen] = useState(false);
+  const [messageSeverity, setMessageSeverity] = useState<AlertColor>("success");
+  const handleChange =
+  (prop: keyof State) => (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  // useEffect(() => {
+  //   if(popupState===false){
+  //     let success = true;
+  //     axiosInstance
+  //       .get("/getNotes", { params: { board_id: params.board_id } })
+  //       .then((res) => {
+  //         if(success){
+  //           console.log(res)
+  //           console.log("Information recieved Successfully");
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log("error getting user boards: ", err);
+  //         success = false;
+  //       });
+  //   }
+  // }, [popupState]);
+  
+  const validateData = () => {
+    errors.name = "";
+    errors.content = "";
 
-  useEffect(() => {
-    axiosInstance
-      .get("/getBoard", { params: { id: params.board_id } })
+    let errorsExits = false;
+    console.log(values.name+' '+ values.content)
+    if (!values.name) {
+      errors.name = "Please enter a valid note Name.";
+      errorsExits = true;
+    }
+
+    if (!values.content) {
+      errors.content = "Please enter a valid note content.";
+      errorsExits = true;
+    }
+    
+    setErrors({ ...errors });
+    return !errorsExits;
+  };
+  const handleSaveNote = () => {
+    
+    if (!validateData()) {
+      return;
+    }
+    console.log(params.board_id)
+    console.log(values)
+    let success = true;
+    axiosInstance.post("/addNote", values, { params: { id: params.board_id } })
       .then((res) => {
-        setBoardName(res.data.board.data.name);
-        console.log("Information recieved Successfully");
-      })
-      .catch((err) => {
-        console.log("error getting user boards: ", err);
+        console.log(res)
+        if (success) {
+          setMessage("Note Added.");
+          setMessageSeverity("success");
+        }else{
+          setMessage("Note cannot be added.");
+          setMessageSeverity("error");
+        }
+      }).catch((err) => {
+        console.log("error adding note: ", err);
+        success = false;
       });
-  }, [params.board_id]);
-
+      values.name = ""
+      values.content = ""
+    setPopupState(false);
+  };
   return (
     <Container sx={{ height: "100vh" }}>
       {popupState ? (
         <div className="overlay">
           <div className="overlayBox">
             <div className="noteHeader">
-              <Typography variant="h6">Note Name</Typography>
+              <Typography variant="h6">New Note</Typography>
               <TextField
                 variant="standard"
                 defaultValue={"Note Name..."}
+                onChange={handleChange("name")}
               ></TextField>
               <Button onClick={() => setPopupState(false)}>
                 <Typography variant="h5">X</Typography>
               </Button>
             </div>
             <div className="noteContent">
-              <textarea></textarea>
+              <textarea onChange={handleChange("content")}></textarea>
             </div>
             <div className="saveDiv">
               <Button
                 className="saveButton"
-                onClick={() => setPopupState(false)}
+                onClick={handleSaveNote}
               >
                 Save Note
               </Button>
@@ -138,6 +227,15 @@ const Notes: React.FC = () => {
           </Box>
         </Box>
       </Box>
+      <Snackbar
+        open={messageOpen}
+        autoHideDuration={6000}
+        onClose={() => setMessageOpen(false)}
+      >
+        <Alert severity={messageSeverity} sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
