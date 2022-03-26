1631157a7dde5313173223edad05faebc202d144
arpithindukuri <danielarpith@gmail.com>
2022-03-26 15:06:14 -0600

    fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,52 +1,93 @@
 export interface Board {
+  id: string | null;
   name: string;
-  description: String;
+  description: string;
+  memberUserIDs: string;
+  notes: Note[];
+  lists: List[];
 }
 
 export interface Note {
-  name: string;
-  date: string;
+  id: string | null;
   content: string;
+  author: string;
+  timestamp: Timestamp;
+  attachmentURL?: string;
 }
 
 export interface List {
+  id: string | null;
   name: string;
+  items: ListItem[];
 }
 
 export interface ListItem {
+  id: string | null;
   name: string;
   isDone: boolean;
 }
 
 export interface Expense {
+  id: string | null;
   name: string;
-  deadline: string;
-  amount: number;
-  assignee: string;
+  dueDate: Timestamp;
+  amount: Money;
+  assignedUserID: string;
+  balance: Money;
 }
+
 export interface Budget {
+  id: string | null;
   name: string;
-  date: string;
-  assigned: string;
-  balance: number;
+  endDate: string;
+  amount: Money;
+  assignedUserID: string;
+  balance: Money;
 }
 
 export interface Event {
+  id: string | null;
   name: string;
-  date: String;
+  startTime: Timestamp;
+  endTime: Timestamp;
+  description: string;
+  tags: Tag[];
+}
+
+export interface Tag {
+  id: string | null;
+  name: string;
+  /** Hex value of the tag's color */
+  color: string;
+}
+
+export interface PersonalReminder {
+  id: string;
+  name: string;
+  time: Timestamp;
+}
+
+export interface PersonalNote {
+  id: string;
+  name: string;
+  content: string;
 }
 
 export interface User {
-  id: string;
+  id: string | null;
   name: string;
   email: string;
-  birthDay: any;
-  alternativeEmail: string;
+  pictureURL?: string;
+  dateOfBirth: any;
+  alternativeEmail?: string;
   phoneNumber: string;
   overview: string;
-  boards: Array<string>;
-  idToken: string;
+  reminders: PersonalReminder[];
+  notes: PersonalNote[];
 }
 
-/** Number of milliseconds from UNIX Epoch */
-export type Timestamp = Number;
+/** Number of **milliseconds** from UNIX Epoch */
+export type Timestamp = number;
+
+/** Number of CAD cents */
+export type Money = number;
